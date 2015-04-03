9044cc3acb71c61ad817e52b543a433eb127c341
Kevin Noll <kevinnoll90@gmail.com>
2015-04-03 20:14:09 +0200

    [FEATURE] added getter for champion names and images

--- src/main.js
+++ src/main.js
@@ -11,14 +11,30 @@
   	svg : null,
   	field : null,
   	data : null,
+  	mChampionNames : null,
+  	mChampionImageUrls : null,
 
   	init : function(){
+  		this.loadImages();
   		this.setup();
   		this.create();
   	},
 
-  	setup : function(){
+  	loadImages : function(){
   		var that = this;
+  		this.mChampionNames = {};
+  		this.mChampionImageUrls = {};
+  		$.each(_DATA_.participants,function(index,value){
+  			var championId = value.championId;
+			$.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/" + championId + "?champData=info&api_key=08d1d2cc-79c5-4dc2-9aa1-50b000cfcd20", function( data ) {
+			  that.mChampionNames[championId] = data.key;
+			  that.mChampionImageUrls[championId] = "http://ddragon.leagueoflegends.com/cdn/5.6.1/img/champion/" + data.key + ".png";
+			});
+  		})
+ 
+  	},
+
+  	setup : function(){
   		this.width = 800;
   		this.height = 800;
   		this.x = d3.scale.linear()
@@ -31,19 +47,17 @@
   			.scale(this.x);
   		this.yAxis = d3.svg.axis()
   			.scale(this.y);
-  		this.svg = d3.select("body").append("svg")
-    		.attr("width", this.width)
-    		.attr("height", this.height);
-  		this.svg.append("svg:image")
-   			.attr('x',-15)
-   			.attr('y',5)
-   			.attr('width', 800)
-   			.attr('height', 800)
-   			.attr("xlink:href","img/map.jpg")
+  	},
 
-		this.field = this.svg.append("g")
-  			.classed(".field", true);
+  	create : function(){
+  		this.createSVG();
+  		this.createBackgroundImage();
+  		this.createField();
+  		this.createPlayers();
+  	},
 
+  	createPlayers : function(){
+  		var that = this;
   		var aFrames = _DATA_.timeline.frames;
   		var aPlayers = _DATA_.participants;
   		var oPlayersInitial = aFrames[0].participantFrames;
@@ -79,10 +93,26 @@
             })
             .style("stroke-width", 1)
             .style("stroke","black");
-
   	},
 
-  	create : function(){
+  	createBackgroundImage : function(){
+  		this.svg.append("svg:image")
+   			.attr('x',-15)
+   			.attr('y',5)
+   			.attr('width', 800)
+   			.attr('height', 800)
+   			.attr("xlink:href","img/map.jpg")
+  	},
+
+  	createSVG : function(){
+  		this.svg = d3.select("body").append("svg")
+    		.attr("width", this.width)
+    		.attr("height", this.height);
+  	},
+
+  	createField : function(){
+		this.field = this.svg.append("g")
+  			.classed(".field", true);
 
   	}
   }
